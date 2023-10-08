76da7db6889230fec3b76e33bcb55feb25d4f45c
Konstantinos Kritos <82104258+Kkritos@users.noreply.github.com>
2023-10-08 16:10:24 -0400

    Update functions2.py
    Writing functions for merger remnant mass, spin, and kick, using relations from Gerosa & Kesden (2016).

--- functions2.py
+++ functions2.py
@@ -206,7 +206,8 @@ def merger_remnant(m1, m2, chi1, chi2, theta1, theta2, dPhi):
     @out chiRem  : merger remnant BH spin parameter in [0,1]
     @out vGWkick : merger remnant GW kick velocity [km/s]
     """
-    
+
+    '''
     # BBH mass ratio:
     q = np.min([m1, m2]) / np.max([m1, m2])
     
@@ -235,6 +236,11 @@ def merger_remnant(m1, m2, chi1, chi2, theta1, theta2, dPhi):
     
     # Final GW kick:
     vGW_kick = pre.finalkick(theta_p, theta_s, dPhi, q, S_1, S_2, maxkick=False, kms=True, more=False)
+    '''
+    
+    m_rem = remnant_mass(m1, m2, chi1, chi2, theta1, theta2, dPhi)
+    chi_rem = remnant_spin(m1, m2, chi1, chi2, theta1, theta2, dPhi)
+    vGW_kick = remnant_kick(m1, m2, chi1, chi2, theta1, theta2, dPhi)
     
     return m_rem, chi_rem, vGW_kick
 
@@ -654,5 +660,139 @@ def get_SN_kick(mBH, wSN_kick):
     """
     
     return maxwell.rvs(loc=0, scale=np.sqrt(3) * wSN_kick * 1.4/mBH)
+
+def remnant_kick(m1, m2, chi1, chi2, theta1, theta2, dPhi):
+    """
+    Merger remnant kick velocity in km/s.
+    Assumes isotropic spin directions.
+    From Gerosa & Kesdsen (2016) and references therein.
+    
+    Inputs:
+    @in m1: primary mass
+    @in m2: secondary mass
+    @in chi1: dimensionless spin parameter of primary
+    @in chi2: dimensionless spin parameter of secondary
+    @in theta1: angle between orbital ang. mom. and primary spin vector
+    @in theta2: angle between orbital ang. mom. and secondary spin vector
+    @in dPhi: angle between spin projections in orbital plane
+    """
+    
+    q = m2 / m1 if m2 < m1 else m1 / m2
+    
+    eta = q / (1 + q)**2
+    
+    A, B, H, V11, VA, VB, VC, C2, C3, zeeta = \
+    1.2e4, -0.93, 6.9e3, 3677.76, 2481.21, 1792.45, 1506.52, 1140, 2481, 145*np.pi/180
+    
+    cost1, cost2 = np.cos(theta1), np.cos(theta2)
+    sint1, sint2 = np.sqrt(1 - cost1**2), np.sqrt(1 - cost2**2)
+    
+    chi1_para, chi2_para = chi1 * cost1, chi2 * cost2
+    
+    chi1_perp, chi2_perp = chi1 * sint1, chi2 * sint2
+    
+    vm = A * eta**2 * (1 - q) / (1 + q) * (1 + B * eta)
+    
+    Delta_para = (chi1_para - q * chi2_para               ) / (1 + q)
+    Delta_perp = (chi1_perp - q * chi2_perp * np.cos(dPhi)) / (1 + q)
+    
+    chi_para = (chi1_para + q**2 * chi2_para) / (1 + q)**2
+    chi_perp = (chi1_perp + q**2 * chi2_perp * np.cos(dPhi)) / (1 + q)**2
+    
+    vs_perp = H * eta**2 * Delta_para
+    
+    vs_para = 16 * eta**2 * (Delta_perp * (V11 + 2 * VA * chi_para + 4 * VB * chi_para**2 + 8 * VC * chi_para**3) \
+                             + 2 * chi_perp * Delta_para * (C2 + 2 * C3 * chi_para)) \
+    * np.cos(np.random.rand() * 2 * np.pi)
+    
+    vk = np.sqrt(vm**2 + 2 * vm * vs_perp * np.cos(zeeta) + vs_perp**2 + vs_para**2)
+    
+    return vk
+
+def remnant_spin(m1, m2, chi1, chi2, theta1, theta2, dPhi):
+    """
+    Dimensionles merger remnant spin parameter in [0, 1].
+    From Gerosa & Kesdsen (2016) and references therein.
+    
+    Inputs:
+    @in m1: primary mass
+    @in m2: secondary mass
+    @in chi1: dimensionless spin parameter of primary
+    @in chi2: dimensionless spin parameter of secondary
+    @in theta1: angle between orbital ang. mom. and primary spin vector
+    @in theta2: angle between orbital ang. mom. and secondary spin vector
+    @in dPhi: angle between spin projections in orbital plane
+    """
+    
+    q = m2 / m1 if m2 < m1 else m1 / m2
+    
+    eta = q / (1 + q)**2
+    
+    cost1, cost2 = np.cos(theta1), np.cos(theta2)
+    sint1, sint2 = np.sqrt(1 - cost1**2), np.sqrt(1 - cost2**2)
+    
+    chi1_para, chi2_para = chi1 * cost1, chi2 * cost2
+    
+    chi1_perp, chi2_perp = chi1 * sint1, chi2 * sint2
+    
+    Delta_para = (chi1_para - q * chi2_para               ) / (1 + q)
+    Delta_perp = (chi1_perp - q * chi2_perp * np.cos(dPhi)) / (1 + q)
+    
+    chi_para = (chi1_para + q**2 * chi2_para) / (1 + q)**2
+    chi_perp = (chi1_perp + q**2 * chi2_perp * np.cos(dPhi)) / (1 + q)**2
+    
+    chi1_chi2 = chi1_para * chi2_para + chi1_perp * chi2_perp * np.cos(dPhi)
+    
+    chi_squared = (chi1**2 + q**4 * chi2**2 + 2 * q**2 * chi1_chi2) / (1 + q)**4
+    
+    t0, t2, t3, s4, s5 = -2.8904, -3.51712, 2.5763, -0.1229, 0.4537
+    
+    el = 2 * np.sqrt(3) + t2 * eta + t3 * eta**2 + s4 * (1 + q)**4 / (1 + q**2) * chi_squared \
+    + (s5 * eta + t0 + 2) * (1 + q)**2 / (1 + q**2) * chi_para
+    
+    chi_f = np.sqrt(q**2 * el**2 / (1 + q)**4 + chi_squared + q * el * chi_para / (1 + q)**2)
+    
+    chi_f = chi_f if chi_f < 1 else 1
+    
+    return chi_f
+
+def remnant_mass(m1, m2, chi1, chi2, theta1, theta2, dPhi):
+    """
+    Merger remnant mass.
+    From Gerosa & Kesdsen (2016) and references therein.
+    
+    Inputs:
+    @in m1: primary mass
+    @in m2: secondary mass
+    @in chi1: dimensionless spin parameter of primary
+    @in chi2: dimensionless spin parameter of secondary
+    @in theta1: angle between orbital ang. mom. and primary spin vector
+    @in theta2: angle between orbital ang. mom. and secondary spin vector
+    @in dPhi: angle between spin projections in orbital plane
+    """
+    
+    q = m2 / m1 if m2 < m1 else m1 / m2
+    
+    eta = q / (1 + q)**2
+    
+    cost1, cost2 = np.cos(theta1), np.cos(theta2)
+    sint1, sint2 = np.sqrt(1 - cost1**2), np.sqrt(1 - cost2**2)
+    
+    chi1_para, chi2_para = chi1 * cost1, chi2 * cost2
+    
+    chi_para = (chi1_para + q**2 * chi2_para) / (1 + q)**2
+    
+    Z1 = 1 + (1 - chi_para**2)**(1 / 3) * ((1 + chi_para)**(1 / 3) + (1 - chi_para)**(1 / 3))
+    Z2 = np.sqrt(3 * chi_para**2 + Z1**2)
+    
+    r_isco = 3 + Z2 - np.sign(chi_para) * np.sqrt((3 - Z1) * (3 + Z1 + 2 * Z2))
+    
+    E_isco = np.sqrt(1 - 2 / 3 / r_isco)
+    
+    p0, p1 = 0.04827, 0.01707
+    
+    m_f = (m1 + m2) * (1 - eta * (1 - 4 * eta) * (1 - E_isco) - 16 * eta**2 * (p0 + 4 * p1 * chi_para * (chi_para + 1)))
+    
+    return m_f
 
 # end of file
