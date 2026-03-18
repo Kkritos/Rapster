716e786a7ec82059a0d52669307e2c7681273855
Konstantinos Kritos <82104258+Sortikostas@users.noreply.github.com>
2026-03-17 22:16:21 -0600

    update constants.py

--- rapster/constants.py
+++ rapster/constants.py
@@ -26,6 +26,7 @@ import time
 import pandas as pd
 from math import erf
 from scipy.optimize import root
+import os
 
 # Use astrophysical units throughout:
 # mass -> solar masses (M_sun)
@@ -57,15 +58,21 @@ Omega_K = 0.0
 # Vacuum density parameter:
 Omega_V = 0.70
 
+# Get the directory where constants.py lives (the 'rapster' folder):
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Go up one level to the root, then into the Data folder:
+DATA_PATH = os.path.join(BASE_DIR, '..', 'Data', 'Planck18_lookup_table.npz')
+
 # load Planck18 lookup table:
-Planck18_lookup_table = np.load('../Data/Planck18_lookup_table.npz')
+Planck18_lookup_table = np.load(DATA_PATH)
 
 # interpolate redshift-lookback and lookback-redshift relations:
 lookback_interp = interpolate.interp1d(Planck18_lookup_table['z'], Planck18_lookup_table['lookback'])
 redshift_interp = interpolate.interp1d(Planck18_lookup_table['t'], Planck18_lookup_table['redshift'])
 
 # Hardening constant:
-Hardening_constant = 4.0 / 7.0
+Hardening_constant = 4.0 /7
 
 # Average number of intermediate BBH states during binary-single interaction:
 N_IMS = 20
